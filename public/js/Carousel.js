b9e39ec709b9a9d6373bf84fd07253b0ad1086e8
MorningStar <792634614@qq.com>
2017-11-03 19:09:48 +0800

    时之沙1.1.0

--- public/js/Carousel.js
+++ public/js/Carousel.js
@@ -1,11 +1,11 @@
-(function($) {//创建一个闭包
-	  
+(function($) { //创建一个闭包
+
 	$.fn.extend({
-		
-		Carousel: function(options) {//插件定义
+
+		Carousel: function(options) { //插件定义
 			var height = $(window).height();
 			var width = $(window).width();
-			var defaultsSetSpace = {//命名空间
+			var defaultsSetSpace = { //命名空间
 				carouselId: '.itemBox',
 				carouselItem: '.carouselItem',
 				autoCarousel: false,
@@ -13,7 +13,7 @@
 				tabCarouselItemTime: 1500,
 				carouselHeight: 0.80
 			};
-			var opts = $.extend(defaultsSetSpace, options);//迭代和格式化每个匹配的元素 
+			var opts = $.extend(defaultsSetSpace, options); //迭代和格式化每个匹配的元素 
 			var carouselId = defaultsSetSpace.carouselId;
 			var carouselItem = defaultsSetSpace.carouselItem;
 			var autoCarousel = defaultsSetSpace.autoCarousel;
@@ -24,11 +24,11 @@
 			$('.carousel_control').css('line-height', carouselHeight * 100 + '%')
 			var i = 0;
 			//轮播控制
-			if(autoCarousel) {//如果autoCarousel为ture执行自动轮播
+			if(autoCarousel) { //如果autoCarousel为ture执行自动轮播
 				setInterval(function() {
-					if(i + 1 < $(carouselId).children().length) {//轮播盒子的子元素个数
+					if(i + 1 < $(carouselId).children().length) { //轮播盒子的子元素个数
 						if(i == 0) {
-							$(carouselId).find('div').eq(0).css('display', 'none');//轮播盒子中，为DIV的子元素被设置为不可见
+							$(carouselId).find('div').eq(0).css('display', 'none'); //轮播盒子中，为DIV的子元素被设置为不可见
 							$(carouselId).find('div').eq(i + 1).fadeIn(tabCarouselItemTime);
 						} else {
 							$(carouselId).find('div').eq(i).css('display', 'none')
@@ -46,33 +46,32 @@
 			$('.right').click(function() {
 				if(i + 1 < $(carouselId).children().length) {
 					if(i == 0) {
-						$(carouselId).find('div').eq(0).css('display', 'none');
-						$(carouselId).find('div').eq(i + 1).fadeIn(tabCarouselItemTime);
+						$($(carouselId).children()[i]).css('display', 'none');
+						$($(carouselId).children()[i + 1]).fadeIn(tabCarouselItemTime);
 					} else {
-						$(carouselId).find('div').eq(i).css('display', 'none')
-						$(carouselId).find('div').eq(i + 1).fadeIn(tabCarouselItemTime);
+						$($(carouselId).children()[i]).css('display', 'none')
+						$($(carouselId).children()[i + 1]).fadeIn(tabCarouselItemTime);
 					}
 					i++;
 				} else {
-					$(carouselId).find('div').eq(i).css('display', 'none');
+					$($(carouselId).children()[i]).css('display', 'none');
 					i = 0;
-					$(carouselId).find('div').eq(i).fadeIn(tabCarouselItemTime);
+					$($(carouselId).children()[i]).fadeIn(tabCarouselItemTime);
 				}
 
 			})
 			$('.left').click(function() {
 				if(i > 0) {
-					$(carouselId).find('div').eq(i).css('display', 'none')
-					$(carouselId).find('div').eq(i - 1).fadeIn(tabCarouselItemTime);
+					$($(carouselId).children()[i]).css('display', 'none')
+					$($(carouselId).children()[i - 1]).fadeIn(tabCarouselItemTime);
 					i--;
 				} else if(i == 0) {
-					$(carouselId).find('div').eq(i).css('display', 'none');
+					$($(carouselId).children()[i]).css('display', 'none');
 					var end = $(carouselId).children().length;
 					end -= 1;
-					$(carouselId).find('div').eq(end).fadeIn(tabCarouselItemTime);
+					$($(carouselId).children()[i]).fadeIn(tabCarouselItemTime);
 					i = end;
 				}
-
 			})
 		}
 
--- public/js/feedback.js
+++ public/js/feedback.js
@@ -1,3 +1,10 @@
+/*!
+ * ======================================================
+ * FeedBack Template For MUI (http://dev.dcloud.net.cn/mui)
+ * =======================================================
+ * @version:1.0.0
+ * @author:cuihongbao@dcloud.io
+ */
 (function() {
 	var index = 1;
 	var size = null;
@@ -5,7 +12,7 @@
 	var starIndex = 0;
 	var feedback = {
 		question: document.getElementById('question'), 
-		
+		contact: document.getElementById('contact'), 
 		imageList: document.getElementById('image-list'),
 		submitBtn: document.getElementById('submit')
 	};
@@ -26,14 +33,13 @@
 			os:  mui.os.version,
 			net: ''+plus.networkinfo.getCurrentType()
 		}
-		localStorage.massageNum = 0+'data';
-		localStorage.imgNum = 0+'url'
 	});
 	/**
 	 *提交成功之后，恢复表单项 
 	 */
 	feedback.clearForm = function() {
-		feedback.question.innerHTML = '';
+		feedback.question.value = '';
+		feedback.contact.value = '';
 		feedback.imageList.innerHTML = '';
 		feedback.newPlaceholder();
 		feedback.files = [];
@@ -126,7 +132,7 @@
 					mui.toast('压缩失败！')
 				});
 				
-			
+
 				
 			}, function(e) {
 				mui.toast(e.message);
@@ -139,20 +145,90 @@
 	};
 	feedback.newPlaceholder();
 	feedback.submitBtn.addEventListener('tap', function(event) {
-		if (feedback.question.innerHTML) {
-			var ma = feedback.question.innerHTML;
-			var mg = feedback.files;
-			mg = JSON.stringify(mg)
-			localStorage[localStorage.massageNum] = ma;
-			localStorage[localStorage.imgNum] = mg;
+		if (feedback.question.value == '' ||
+			(feedback.contact.value != '' &&
+				feedback.contact.value.search(/^(\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+)|([1-9]\d{4,9})$/) != 0)) {
+			return mui.toast('信息填写不符合规范');
 		}
-		if (feedback.question.innerHTML.length > 2000) {
+		if (feedback.question.value.length > 200 || feedback.contact.value.length > 200) {
 			return mui.toast('信息超长,请重新填写~')
 		}
-		if ((!feedback.question.innerHTML || feedback.question.innerHTML == '')&&feedback.files.length==0)
-		{
-			return mui.toast('内容不能为空')
+		//判断网络连接
+		if(plus.networkinfo.getCurrentType()==plus.networkinfo.CONNECTION_NONE){
+			return mui.toast("连接网络失败，请稍后再试");
 		}
-		
+		feedback.send(mui.extend({}, feedback.deviceInfo, {
+			content: feedback.question.value,
+			contact: feedback.contact.value,
+			images: feedback.files,
+			score:''+starIndex
+		})) 
 	}, false)
+	feedback.send = function(content) {
+		feedback.uploader = plus.uploader.createUpload(url, {
+			method: 'POST'
+		}, function(upload, status) {
+//			plus.nativeUI.closeWaiting()
+			console.log("upload cb:"+upload.responseText);
+			if(status==200){
+				var data = JSON.parse(upload.responseText);
+				//上传成功，重置表单
+				if (data.ret === 0 && data.desc === 'Success') {
+//					mui.toast('反馈成功~')
+					console.log("upload success");
+//					feedback.clearForm();
+				}
+			}else{
+				console.log("upload fail");
+			}
+			
+		});
+		//添加上传数据
+		mui.each(content, function(index, element) {
+			if (index !== 'images') {
+				console.log("addData:"+index+","+element);
+//				console.log(index);
+				feedback.uploader.addData(index, element)
+			} 
+		});
+		//添加上传文件
+		mui.each(feedback.files, function(index, element) {
+			var f = feedback.files[index];
+			console.log("addFile:"+JSON.stringify(f));
+			feedback.uploader.addFile(f.path, {
+				key: f.name
+			});
+		});
+		//开始上传任务
+		feedback.uploader.start();
+		mui.alert("感谢反馈，点击确定关闭","问题反馈","确定",function () {
+			feedback.clearForm();
+			mui.back();
+		});
+//		plus.nativeUI.showWaiting();
+	};
+	
+	 //应用评分
+	 mui('.icons').on('tap','i',function(){
+	  	var index = parseInt(this.getAttribute("data-index"));
+	  	var parent = this.parentNode;
+	  	var children = parent.children;
+	  	if(this.classList.contains("mui-icon-star")){
+	  		for(var i=0;i<index;i++){
+  				children[i].classList.remove('mui-icon-star');
+  				children[i].classList.add('mui-icon-star-filled');
+	  		}
+	  	}else{
+	  		for (var i = index; i < 5; i++) {
+	  			children[i].classList.add('mui-icon-star')
+	  			children[i].classList.remove('mui-icon-star-filled')
+	  		}
+	  	}
+	  	starIndex = index;
+  });
+  	//选择快捷输入
+	mui('.mui-popover').on('tap','li',function(e){
+	  document.getElementById("question").value = document.getElementById("question").value + this.children[0].innerHTML;
+	  mui('.mui-popover').popover('toggle')
+	}) 
 })();
